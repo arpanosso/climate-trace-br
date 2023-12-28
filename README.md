912234959158cacb0f0cc8f01f28ce9359d0be20
Alan Rodrigo Panosso <arpanosso@yahoo.com.br>
2023-12-28 18:14:05 -0300

    novo

--- README.md
+++ README.md
@@ -7,4 +7,106 @@
 
 ![](img/img-01.png) ![](img/img-02.png) ![](img/img-03.png)
 
-![](img/img-04.png)
+![](img/img-04.png) \## Carregando Pacotes
+
+``` r
+library(tidyverse)
+```
+
+## Mesclando base com ids
+
+``` r
+brazil_ids <- read_rds("data/df_nome.rds")
+emissions_sources <- read_rds("data/emissions_sources.rds")
+```
+
+``` r
+# source_name <- emissions_sources %>% 
+#   pull(source_name) %>% 
+#   unique()
+# 
+# brazil_ids_name <- brazil_ids %>% 
+#   pull(nome) %>% 
+#   unique()
+# 
+# flag_name <- source_name %in% brazil_ids_name
+# 
+# sources_city <- emissions_sources %>% 
+#   dplyr::filter(source_name %in% brazil_ids_name)
+
+emissions_sources <- left_join(emissions_sources,
+          brazil_ids %>% 
+  rename(source_name = nome) %>% 
+    select(source_name,sigla_uf,nome_uf,nome_regiao),
+  by = "source_name")
+```
+
+``` r
+uf_nome <- brazil_ids$nome_uf %>% unique()
+# emissions_sources %>% 
+#   filter(!source_name %in% uf_nome,
+#          !source_name == "Brazil")
+```
+
+``` r
+emissions_sources %>% 
+  filter(sigla_uf == "SP",
+         source_name == "Jaboticabal",
+         sector_name == "agriculture",
+         gas == "co2e_100yr",
+         year == 2022
+         ) %>% 
+  group_by(original_inventory_sector) %>% 
+  summarise( emission = sum(emissions_quantity,
+                            na.rm = TRUE))
+#> # A tibble: 4 × 2
+#>   original_inventory_sector           emission
+#>   <chr>                                  <dbl>
+#> 1 cropland-fires                       904747.
+#> 2 enteric-fermentation-cattle-pasture    3352.
+#> 3 manure-left-on-pasture-cattle           965.
+#> 4 synthetic-fertilizer-application       3568.
+```
+
+``` r
+emissions_sources %>% 
+  filter(sigla_uf == "SP",
+         source_name == "Jaboticabal",
+         sector_name == "forestry",
+         gas == "co2e_100yr",
+         year == 2022
+         ) %>%
+  group_by(sub_sector) %>% 
+  summarise( emission = sum(emissions_quantity,
+                            na.rm = TRUE))
+#> # A tibble: 8 × 2
+#>   sub_sector              emission
+#>   <chr>                      <dbl>
+#> 1 forest-land-clearing      3422. 
+#> 2 forest-land-degradation     20.5
+#> 3 forest-land-fires          953. 
+#> 4 net-forest-land         -17490. 
+#> 5 net-shrubgrass            -756. 
+#> 6 net-wetland               -511. 
+#> 7 removals                -14783. 
+#> 8 shrubgrass-fires          6175.
+```
+
+``` r
+states_id <- brazil_ids$sigla_uf %>% unique()
+emissions_sources %>% 
+  filter(
+    #source_name != "Acre",
+    #sigma_uf == "SP", # %in% states_id,
+    sector_name == "agriculture",
+    gas == "co2e_100yr",
+    year == 2022
+  ) %>% 
+  group_by(lon,lat) %>% 
+  summarise(emissions_quantity = sum(emissions_quantity,na.rm=TRUE)) %>% 
+  arrange(desc(emissions_quantity)) %>% 
+  ggplot(aes(lon,lat,size=emissions_quantity)) +
+  geom_point(color="black", fill= "blue",shape=21)
+```
+
+![](README_files/figure-gfm/unnamed-chunk-8-1.png)<!-- -->
--- data-raw/faxina-metadados.R
+++ data-raw/faxina-metadados.R
@@ -1,60 +1,62 @@
 library(tidyverse)
 
 # buscando o caminho dos setores
+tbl_directorys <- as_tibble(
+  list.files("data-raw/BRA/",
+             full.names = TRUE,
+             recursive = TRUE)
+) %>%
+  filter(str_detect(value,"agriculture|forestry_and_land_use")) %>%
+  filter(str_detect(value, "emissions-sources.csv"))
+
 list_sector <- list.files("data-raw/BRA/",
-           full.names = TRUE,
-           pattern = "agriculture|forestry")
+                          full.names = TRUE,
+                          pattern = "agriculture|forestry")
 
-padroes <- c("country","confidence","sources.csv")
-
-
-list_sector_source <- c(list.files(list_sector[1],
-          pattern = "sources.csv",
-           full.names =TRUE)[-1],
-list.files(list_sector[2],
-          pattern = "sources.csv",
-           full.names =TRUE)[-1])
+# padroes <- c("country","confidence","sources.csv")
+#
+# list_sector_source <- c(list.files(list_sector[1],
+#           pattern = "sources.csv",
+#            full.names =TRUE)[-1],
+# list.files(list_sector[2],
+#           pattern = "sources.csv",
+#            full.names =TRUE)[-1])
 
 # list_sector_source <- list_sector_source[-(1:2)]
 
 my_file_stack <- function(sector_name){
-  read.csv(sector_name) %>%
-        select(!starts_with("other"))
+  names <- read.csv(sector_name) %>%
+    select(!starts_with("other")) %>% #names()
+    mutate(directory = sector_name)
+  # print(list(sector_name,
+  #            names))
 }
-lvec <- str_remove(list_sector_source,".csv")
-dados <- map_dfr(list_sector_source,my_file_stack,
-      .id="list_sector_source") %>%
-  mutate(
-    list_sector_source =case_when(
-      list_sector_source == "1" ~ lvec[1],
-      list_sector_source == "2" ~ lvec[2],
-      list_sector_source == "3" ~ lvec[3],
-      list_sector_source == "4" ~ lvec[4],
-      list_sector_source == "5" ~ lvec[5],
-      list_sector_source == "6" ~ lvec[6],
-      list_sector_source == "7" ~ lvec[7],
-      list_sector_source == "8" ~ lvec[8],
-      list_sector_source == "9" ~ lvec[9],
-      list_sector_source == "10" ~ lvec[10],
-      list_sector_source == "11" ~ lvec[11],
-      list_sector_source == "12" ~ lvec[12],
-      list_sector_source == "13" ~ lvec[13],
-      list_sector_source == "14" ~ lvec[14],
-      list_sector_source == "15" ~ lvec[15],
-    )
-  )
+
+value <- tbl_directorys %>% pull(value)
+my_file_stack(value[1])
+# map(value,my_file_stack)
+ldados <- map_dfr(value,my_file_stack)
 glimpse(dados)
 
 dados <- dados %>%
   mutate(
-    sector = str_split(list_sector_source,
-                       "/|_",
-                       simplify = TRUE)[,3],
-    sub_sector = str_split(list_sector_source,
-                       "/|_",
-                       simplify = TRUE)[,4],
-    type = str_split(list_sector_source,
-                     "/|_",
-                     simplify = TRUE)[,5],
+    start_time = as_date(start_time),
+    end_time = as_date(end_time),
+    created_date = as_date(created_date),
+    modified_date = as_date(modified_date),
+    year = lubridate::year(end_time)
+  ) %>%
+  mutate(
+    sector_name = str_split(directory,
+                            "/|_",
+                            simplify = TRUE)[,3],
+    sub_sector = str_split(directory,
+                           "/",
+                           simplify = TRUE)[,4],
+    sub_sector = str_remove(sub_sector,"_emissions-sources.csv")
   )
+write_rds(dados, "data/emissions_sources.rds")
+
+
+
 
